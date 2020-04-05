65ed47e4621db0fc5febbe3078c91757c89e3acf
ericlima <eric.george.lima@gmail.com>
2020-04-05 05:51:48 +0100

    1 file modified

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   state = {
     term: "",
     API_KEY: "535accbd0bb3df7f9f65eab220b6d317",
-    url: "https://www.nif.pt/?json=1&q={NIF}&key={API_KEY}"
+    url: "https://www.nif.pt/?json=1&q={NIF}&key={API_KEY}",
+    credito: "0,00"
   };
 
   constructor(props) {
@@ -15,6 +16,15 @@ class App extends React.Component {
   onInputChange = event => {
     this.setState({ term: event.target.value });
   };
+
+  consultaCreditos = async event => {
+    const url = "https://www.nif.pt/?json=1&credits=1&key=key"
+      .replace("key",this.state.API_KEY);
+
+    const response = await axios.get(url,{ mode: 'no-cors'});
+
+    this.setState({ credito: response.credits })
+  }
 
   onFormSubmit = async event => {
 
@@ -40,7 +50,8 @@ class App extends React.Component {
       <div className="search-bar ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Consulta NIF Portugues</label>
+            <label>Consulta NIF Portugues</label> 
+            <a onClick={this.consultaCreditos} >Consulta Créditos {this.state.credito}</a>
             <input
               type="text"
               value={this.state.term}
